dc032e74f7d6233c6ee6672f0339fce5c0cbbb54
yuuki <yuuki@yuukikonno.com>
2025-09-10 15:43:35 +0900

    feat: initial commit

---
+++ ly-to-all.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+soundfont="$HOME/Library/Audio/Sounds/Banks/FluidR3_GM.sf2"
+input="${1%.ly}"
+
+# Exit on error
+set -e
+
+# Convert LilyPond to SVG and MIDI
+lilypond --svg -dcrop -dmidi-extension=mid "$input.ly"
+mv "$input.cropped.svg" "$input.svg"
+
+# Set the SVG background to white
+rsvg-convert -b white -f svg -o "$input.svg" "$input.svg"
+
+# Convert MIDI to WAV
+fluidsynth -ni "$soundfont" "$input.mid" -F "$input.wav"
+
+# Convert WAV to WebM
+ffmpeg -y -f lavfi -i 'color=c=black:s=320x180' -i "$input.wav" -c:v libvpx-vp9 -c:a libopus -af 'silenceremove=stop_periods=1:stop_threshold=-50dB' -shortest -pix_fmt yuv420p "$input.webm"
+
+rm "$input.wav"
+
+# Convert LilyPond to MusicXML (extracting only the `\relative` block)
+python3 -c 'import re, sys; print(re.search(r"\\relative.*?{.*?}", open(sys.argv[1]).read(), re.DOTALL).group(0))' "$input.ly" \
+| ly musicxml -d 'backup-suffix=' -o "$input.musicxml"
---
+++ row/row.ly
@@ -0,0 +1,33 @@
+\version "2.24.4"
+
+\paper {
+  indent = #0
+  line-width = #120
+}
+
+\header {
+  title = "Row, Row, Row Your Boat"
+}
+
+\score {
+  <<
+  \chords {
+    c,2.*4 |
+    c,2.*2 | g,2. | c,2.
+  }
+  \relative c' {
+    \time 6/8
+    c4. c4. | c4 d8 e4. | e4 d8 e4 f8 | g2. | \break
+    c8[ c8 c8] g8[ g8 g8] | e8[ e8 e8] c8[ c8 c8] | g'4 f8 e4 d8 | c2. \fine
+  }
+  >>
+
+  \layout {
+    \autoBreaksOff
+    \numericTimeSignature
+  }
+
+  \midi {
+    \tempo 4. = 120
+  }
+}
