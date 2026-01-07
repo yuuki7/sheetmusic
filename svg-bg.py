2add7524eb9b00dc98214ddc12d7c85fe9209a8e
yuuki <yuuki@yuukikonno.com>
2026-01-07 23:56:30 +0900

    chore: update

--- svg-bg.py
+++ svg-bg.py
@@ -2,6 +2,12 @@
 import sys
 import xml.etree.ElementTree as ET
 
-dom = ET.parse(sys.argv[1])
+input_filename = sys.argv[1]
+
+ET.register_namespace('', 'http://www.w3.org/2000/svg')
+ET.register_namespace('xlink', 'http://www.w3.org/1999/xlink')
+
+dom = ET.parse(input_filename)
 svg = dom.getroot()
 svg.insert(0, ET.fromstring('<rect width="100%" height="100%" fill="#ffffff"/>'))
+dom.write(input_filename)
