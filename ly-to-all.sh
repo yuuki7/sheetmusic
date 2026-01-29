483b8d028feee06e1c72cd03055b477dddb10fcf
yuuki <yuuki@yuukikonno.com>
2026-01-29 16:15:16 +0900

    feat(music): add WebP and Opus (#5)
    - chore(scripts): add WebP
    - chore(scripts): add Opus
    - feat(music): add WebP and Opus

--- ly-to-all.sh
+++ ly-to-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Convert all LilyPond files to SVG, MIDI, WAV, and WebM
+# Convert all LilyPond files to SVG, WebP, MIDI, WAV, Opus, and WebM
 #
 
 # Exit on error
@@ -23,6 +23,10 @@ for ly_file in ${1:-*}/*.ly; do
   # Optimize SVG
   npx svgo --quiet "$file.svg"
 
+  # Convert SVG to WebP with white background
+  rsvg-convert --width=2160 --background-color=white \
+    --output="$file.webp" "$file.svg"
+
   # Set SVG background to white
   ./svg-bg.js "$file.svg"
 
@@ -32,9 +36,12 @@ for ly_file in ${1:-*}/*.ly; do
   # Convert MIDI to WAV
   fluidsynth --quiet --fast-render="$file.wav" "$SOUNDFONT" "$file.mid"
 
-  # Convert WAV to WebM
+  # Convert WAV to Opus
+  ffmpeg -y -loglevel error -i "$file.wav" -codec:a libopus "$file.opus"
+
+  # Convert Opus to WebM
   ffmpeg -y -loglevel error \
-    -f lavfi -i 'color=color=black:size=320x180' -i "$file.wav" \
+    -f lavfi -i 'color=color=black:size=256x144' -i "$file.opus" \
     -codec:v libvpx-vp9 -codec:a libopus \
     -filter:a 'silenceremove=stop_periods=1:stop_threshold=-50dB' \
     -shortest -pix_fmt yuv420p "$file.webm"
